87998aa4a37ec4917b25887eac74ec64b0451ce2
Lucian Plesea <lucianpls@gmail.com>
2019-05-09 11:46:04 -0700

    A few directives and started the handler

--- src/mod_athse_png.cpp
+++ src/mod_athse_png.cpp
@@ -10,11 +10,17 @@
 #include <receive_context.h>
 
 using namespace std;
+
 NS_AHTSE_USE
 
 #define CMD_FUNC (cmd_func)
+
+extern module AP_MODULE_DECLARE_DATA ahtse_png_module;
+
 struct png_conf {
     apr_array_header_t *arr_rxp;
+    int indirect;  // Subrequests only
+    int only;      // Block non-pngs
 };
 
 static void *create_dir_config(apr_pool_t *p, char *dummy) {
@@ -23,13 +29,53 @@ static void *create_dir_config(apr_pool_t *p, char *dummy) {
     return c;
 }
 
-static const command_rec cmds[] = {
-    {NULL}
-};
+static const char *set_regexp(cmd_parms *cmd, png_conf *c, const char *pattern)
+{
+    return add_regexp_to_array(cmd->pool, &c->arr_rxp, pattern);
+}
 
 static int handler(request_rec *r) {
+    if (r->method_number != M_GET)
+        return DECLINED;
+
+    png_conf *cfg = get_conf<png_conf>(r, &ahtse_png_module);
+    if ((cfg->indirect && !r->main)
+        || !requestMatches(r, cfg->arr_rxp))
+        return DECLINED;
+
+    // Our request
+    sz tile;
+    if (APR_SUCCESS != getMLRC(r, tile))
+        return HTTP_BAD_REQUEST;
+
     return DECLINED;
 }
+
+static const command_rec cmds[] = {
+    AP_INIT_TAKE1(
+        "AHTSE_PNG_RegExp",
+        CMD_FUNC set_regexp,
+        0, // self pass arg
+        ACCESS_CONF,
+        "The request pattern the URI has to match"
+    )
+    ,AP_INIT_FLAG(
+        "AHTSE_PNG_Indirect",
+        CMD_FUNC ap_set_flag_slot,
+        (void *)APR_OFFSETOF(png_conf, indirect),
+        ACCESS_CONF, // availability
+        "If set, module only activates on subrequests"
+    )
+    ,AP_INIT_FLAG(
+        "AHTSE_PNG_Only",
+        CMD_FUNC ap_set_flag_slot,
+        (void *)APR_OFFSETOF(png_conf, only),
+        ACCESS_CONF, // availability
+        "If set, non-png files are blocked and reported as warnings."
+        " Default is off, allowing them to be sent"
+    )
+    ,{NULL}
+};
 
 static void register_hooks(apr_pool_t *p) {
     ap_hook_handler(handler, NULL, NULL, APR_HOOK_LAST);
